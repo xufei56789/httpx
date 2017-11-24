f994490483e1ca0e9ab2d00f815466ba448f919b
Yi Liu <yiliu@email.com>
2017-11-24 18:38:49 +0800

    创建httpx库

---
+++ httpx/config.go
@@ -0,0 +1,55 @@
+package httpx
+
+import (
+	"net/url"
+	"net/http"
+	"errors"
+	"strconv"
+	"crypto/x509"
+)
+
+//设置代理(proxyUrl [代理地址http://xxxxx:prot])
+func (httpx *Httpx) SetProxy(proxyUrl string) error {
+	u := url.URL{}
+	urlproxy, err := u.Parse(proxyUrl)
+	if err != nil {
+		return err
+	}
+	httpx.configTr.Proxy = http.ProxyURL(urlproxy)
+	return nil
+}
+
+//设置重定向回调函数
+func (httpx *Httpx) SetRedirectFunc(f func(req *http.Request, via []*http.Request) (e error)) {
+	httpx.client.CheckRedirect = f
+}
+
+//重定向次数设定 (i[0 停止重定向, > 0 重定向次数])
+func (httpx *Httpx) SetRedirect(i int) {
+	//声明闭包
+	f := func(req *http.Request, via []*http.Request) (e error) {
+		//如果 i = 0 不重定向
+		if i <= 0 {
+			return http.ErrUseLastResponse
+		}
+		//如果 当前重定向次数 >= 重定向次数停止
+		if len(via) >= i {
+			return errors.New("stopped after "+strconv.Itoa(i)+" redirects")
+		}
+		return nil
+	}
+	//设置重定向回调函数
+	httpx.SetRedirectFunc(f)
+}
+
+//是否跳过证书检查
+func (httpx *Httpx) SetInsecureSkipVerify(b bool)  {
+	httpx.configTls.InsecureSkipVerify = true
+}
+
+//设置x509cert证书
+func (httpx *Httpx) SetCertPoolx509(b []byte)  {
+	pool := x509.NewCertPool()
+	pool.AppendCertsFromPEM(b)
+	httpx.configTls.RootCAs = pool
+}
---
+++ httpx/httpx.go
@@ -0,0 +1,55 @@
+package httpx
+
+import (
+	"net/http"
+	"net/url"
+	"crypto/tls"
+)
+
+type Httpx struct {
+	//http客户端指针
+	client *http.Client
+	//协议头指针
+	header *http.Header
+	//配置指针
+	configTr *http.Transport
+	configTls *tls.Config
+}
+
+type Response struct {
+	//Body
+	Body []byte
+	//Length
+	Length int64
+	//status code
+	Status int
+	//Headers
+	Header http.Header
+	//Cookies
+	Cookies []*http.Cookie
+	//URL
+	Url *url.URL
+}
+
+const (
+	GET     = "GET"
+	POST    = "POST"
+	PUT     = "PUT"
+	DELETE  = "DELETE"
+	HEAD    = "HEAD"
+	TRACE   = "TRACE"
+	CONNECT = "CONNECT"
+)
+
+//创建httpx指针对象
+func NewHttpx() *Httpx {
+	var httpx Httpx
+	//声明配置
+	tr := &http.Transport{}
+	tr.TLSClientConfig = &tls.Config{}
+	httpx.client = &http.Client{}
+	httpx.configTr = tr
+	httpx.configTls = tr.TLSClientConfig
+	httpx.client.Transport = tr
+	return &httpx
+}
